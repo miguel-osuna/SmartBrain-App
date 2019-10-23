4d33f86a18fdc42e7a5c9b77b821f10e320d6af5
miguel-osuna <osuna.angulo.miguel@gmail.com>
2019-10-23 16:06:55 -0700

    Updates

---
+++ precache-manifest.e1bc1bd9281c911d69e5b4ea77106cd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c3f5cce6280e3a46bd3b82cb78510c",
+    "url": "/miguel-osuna/SmartBrain-App/index.html"
+  },
+  {
+    "revision": "b92cceb1dceae0d738f2",
+    "url": "/miguel-osuna/SmartBrain-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "328041759306433d90f5",
+    "url": "/miguel-osuna/SmartBrain-App/static/css/main.93cd76d9.chunk.css"
+  },
+  {
+    "revision": "b92cceb1dceae0d738f2",
+    "url": "/miguel-osuna/SmartBrain-App/static/js/2.54f54608.chunk.js"
+  },
+  {
+    "revision": "328041759306433d90f5",
+    "url": "/miguel-osuna/SmartBrain-App/static/js/main.faeaa54c.chunk.js"
+  },
+  {
+    "revision": "99547b9f6c28a5150561",
+    "url": "/miguel-osuna/SmartBrain-App/static/js/runtime-main.6f1bc00b.js"
+  },
+  {
+    "revision": "584371a13f33dcafd09ef879d2d53393",
+    "url": "/miguel-osuna/SmartBrain-App/static/media/brain.584371a1.png"
+  }
+]);
